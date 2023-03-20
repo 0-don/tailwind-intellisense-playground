39c5d6c50d6fbd7a3a2c18007f90dc2fe97c4ad4
Don-Cryptus <don.cryptus@gmail.com>
2023-03-20 19:54:53 +0100

    adasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
